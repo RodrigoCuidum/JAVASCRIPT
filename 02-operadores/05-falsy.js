d4bf032df7a1c42f02b09bbe9092c9554b91f2f7
Rodrigo Mendez <rodrigo.mendez@cuidum.com>
2024-10-21 10:46:45 +0200

    ADD operadores

---
+++ 02-operadores/05-falsy.js
@@ -0,0 +1,25 @@
+// short-circuit
+
+// Falso (FALSY)
+
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
+let nombre = '';
+let username = nombre || 'Anónimo';
+console.log(username);
+
+function fn1() {
+    console.log('soy funcion 1');
+    return false;
+}
+
+function fn2() {
+    console.log('soy funcion 2');
+    return true;
+}
+
+let x = fn1() && fn2();
