ed00709e4def5389cfe7deb49e1e439cc298d21e
Rodrigo Mendez <rodrigo.mendez@cuidum.com>
2024-10-21 12:38:42 +0200

    ADD control de flujo

---
+++ 03-control-flujo/05-do-while.js
@@ -0,0 +1,16 @@
+let i = 2;
+// Comprueba la condición y luego ejecuta el código...
+// while (i < 10) {
+//    if (i % 2 == 0) {
+//    }
+//    console.log('Número par', i);
+//    i++;
+//}
+
+// Ejecuta el código y luego comprueba la condición...
+do {
+    if (i % 2 == 0) {
+    }
+    console.log('Número par', i);
+    i++;
+} while (i < 10);
